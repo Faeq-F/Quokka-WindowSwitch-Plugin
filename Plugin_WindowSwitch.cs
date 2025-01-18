d68069ae84baaa0d666e4a2d1d8d0cfaabc76cc7
Faeq <faeqfaisal@hotmail.co.uk>
2025-01-18 12:49:52 +0000

    :sparkles: added settings and implemented fuzzy search, blacklist, special command & signifier

--- Plugin_WindowSwitch.cs
+++ Plugin_WindowSwitch.cs
@@ -1,8 +1,11 @@
 
+using Newtonsoft.Json;
 using Quokka.ListItems;
 using Quokka.PluginArch;
 using System.Diagnostics;
+using System.IO;
 using System.Runtime.InteropServices;
+using WinCopies.Util;
 
 namespace Plugin_WindowSwitch {
   /// <summary>
@@ -19,18 +22,40 @@ namespace Plugin_WindowSwitch {
     /// </summary>
     public override string PluggerName { get; set; } = "WindowSwitch";
 
+    private static Settings pluginSettings = new();
+    internal static Settings PluginSettings { get => pluginSettings; set => pluginSettings = value; }
+
+    /// <summary>
+    /// Loads plugin settings
+    /// </summary>
+    public WindowSwitch() {
+      string fileName = Environment.CurrentDirectory + "\\PlugBoard\\Plugin_WindowSwitch\\Plugin\\settings.json";
+      PluginSettings = JsonConvert.DeserializeObject<Settings>(File.ReadAllText(fileName))!;
+    }
+
+
+    private List<ListItem> ProduceItems(string query) {
+      List<ListItem> items = new();
+      foreach (window w in openWindows) {
+        if (( w.ProcessName.Contains(query, StringComparison.OrdinalIgnoreCase)
+          || w.Title.Contains(query, StringComparison.OrdinalIgnoreCase)
+                || FuzzySearch.LD(w.ProcessName, query) < PluginSettings.FuzzySearchThreshold
+                || FuzzySearch.LD(w.Title, query) < PluginSettings.FuzzySearchThreshold ) && !PluginSettings.BlackList.Contains(w.Title)
+                && !PluginSettings.BlackList.Contains(w.ProcessName)
+                ) {
+          items.Add(new WindowItem(w));
+        }
+      }
+      return items;
+    }
+
+
     /// <summary>
     /// <inheritdoc/>
     /// </summary>
     /// <param name="query"><inheritdoc/></param>
-    /// <returns></returns>
-    public override List<ListItem> OnQueryChange(string query) {
-      List<ListItem> items = new();
-      foreach (window w in openWindows) {
-        items.Add(new WindowItem(w));
-      }
-      return items;
-    }
+    /// <returns>Matching windows that do not have their ProcessName or Title in the blacklist</returns>
+    public override List<ListItem> OnQueryChange(string query) { return ProduceItems(query); }
 
     /// <summary>
     /// Indexes all open windows
@@ -52,7 +77,62 @@ namespace Plugin_WindowSwitch {
         }
       }
     }
+
+
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns>
+    /// The WindowSwitchSignifier from plugin settings
+    /// </returns>
+    public override List<string> CommandSignifiers() {
+      return new List<string>() { PluginSettings.WindowSwitchSignifier };
+    }
+
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="command">The WindowSwitchSignifier (Since there is only 1 signifier for this plugin), followed by the window being searched for</param>
+    /// <returns>List of windows that possibly match what is being searched for (windows that do not have their ProcessName or Title in the blacklist)</returns>
+    public override List<ListItem> OnSignifier(string command) {
+      command = command.Substring(PluginSettings.WindowSwitchSignifier.Length);
+      return ProduceItems(command);
+    }
+
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <returns>The AllWindowsSpecialCommand from plugin settings</returns>
+    public override List<string> SpecialCommands() {
+      List<string> SpecialCommand = new() {
+        PluginSettings.AllWindowsSpecialCommand
+      };
+      return SpecialCommand;
+    }
+
+    /// <summary>
+    /// <inheritdoc/>
+    /// </summary>
+    /// <param name="command">The AllWindowsSpecialCommand (Since there is only 1 special command for this plugin)</param>
+    /// <returns>All windows (that do not have their ProcessName or Title in the blacklist) sorted alphabetically</returns>
+    public override List<ListItem> OnSpecialCommand(string command) {
+      List<ListItem> AllList = new();
+      foreach (window w in openWindows) {
+        if (!PluginSettings.BlackList.Contains(w.Title)
+                && !PluginSettings.BlackList.Contains(w.ProcessName)) {
+          AllList.Add(new WindowItem(w));
+        }
+      }
+      AllList = AllList.OrderBy(x => x.Name).ToList();
+      return AllList;
+    }
   }
+
+
+
 
   internal class window {
     public IntPtr handle;
@@ -67,4 +147,9 @@ namespace Plugin_WindowSwitch {
     }
   }
 
+
+
 }
+
+
+
---
+++ Settings.cs
@@ -0,0 +1,31 @@
+﻿namespace Plugin_WindowSwitch {
+
+
+  /// <summary>
+  ///   The settings for this (Gitmoji) plugin
+  /// </summary>
+  public class Settings {
+
+    /// <summary>
+    /// The command to see all windows (defaults to "AllWindows")
+    /// </summary>
+    public string AllWindowsSpecialCommand { get; set; } = "AllWindows";
+    /// <summary>
+    /// The command signifier used to obtain windows (defaults to "-> ")
+    /// </summary>
+    public string WindowSwitchSignifier { get; set; } = "-> ";
+    /// <summary>
+    ///   List of window titles or process names for windows to not show (defaults to
+    ///   empty - all windows can be shown)
+    /// </summary>
+    public List<string> BlackList { get; set; } = new List<string>(new string[] { });
+    /// <summary>
+    ///   The threshold for when to consider a window title or process name
+    ///   is similar enough to the query for it to be
+    ///   displayed (defaults to 5). Currently uses the
+    ///   Levenshtein distance; the larger the number, the
+    ///   bigger the difference.
+    /// </summary>
+    public int FuzzySearchThreshold { get; set; } = 5;
+  }
+}
